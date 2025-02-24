82d2fe8b48bf7bb1220387604943e1d10b98e37f
Prateek-9814 <pkulkarni9814@gmail.com>
2025-02-24 13:17:17 +0530

    terraform code updated

---
+++ task-1/var.tf
@@ -0,0 +1,24 @@
+
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "zone1"{
+  default = "ap-south-1a"
+}
+
+variable "zone2"{
+  default = "ap-south-1b"
+}
+
+variable "zone3"{
+  default = "ap-south-1c"
+}
+
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  default = "myfirstvpc"
+}
