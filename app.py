ee2cf5df3aec7a67d474053f5d00ee4a81c397f9
prateek-9814 <pkulkarni9814@gmail.com>
2025-02-26 05:45:05 +0000

    python simple file created

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello, Kubernetes!"
+    return "Hello, this is a simple deployment assignment!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
